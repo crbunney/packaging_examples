faa136b01ea1b74595a81e0dc6777dd8f72be04b
Chris R Bunney <crbunney@gmail.com>
2019-11-13 19:51:13 +0000

    Configured cheese project for packaging using setuptools and installation using pip

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+# For now, a setup.py file containing a call to setup() is required. Although that requirement may change in the future
+setup()  # Configuration is done in setup.cfg
